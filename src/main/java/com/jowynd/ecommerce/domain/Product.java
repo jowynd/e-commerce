61ac8b5ab0da7ed4bb47388a796675ab41f3bdf3
jowynd <jowynd12@gmail.com>
2025-02-08 18:09:55 -0300

    Adding Product class to represent an entity

---
+++ src/main/java/com/jowynd/ecommerce/domain/Product.java
@@ -0,0 +1,31 @@
+package com.jowynd.ecommerce.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+}
