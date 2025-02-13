37d5ac8f868fd4fe11180986985d69632ec4c8b9
jowynd <jowynd12@gmail.com>
2025-02-12 22:21:08 -0300

    Adding User class

---
+++ src/main/java/com/jowynd/ecommerce/domain/User.java
@@ -0,0 +1,27 @@
+package com.jowynd.ecommerce.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
