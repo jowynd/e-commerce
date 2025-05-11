4949a9ca4fce0e7c06f57bae79c626e5062e529d
jowynd <jowynd12@gmail.com>
2025-05-10 22:56:17 -0300

    Creating login page

---
+++ frontend/script.js
@@ -0,0 +1,32 @@
+document.getElementById("login-form").addEventListener("submit", async function (event) {
+
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+        const response = await fetch("http://localhost:8080/auth/login", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({ username, password})
+        });
+
+        if (!response.ok) {
+            throw new Error("Invalid email or username");
+        }
+
+        const data = await response.json();
+        const token = data.token;
+
+        localStorage.setItem("token", token);
+        window.location.href = "dashboard.html";
+
+    } catch(error) {
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.textContent = error.message;
+    }
+})
